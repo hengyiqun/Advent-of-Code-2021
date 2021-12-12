5cf010075ad5e4efeec7ec701a17fd5cd8d03505
hengyiqun <hengyiqun@gmail.com>
2021-12-12 19:26:53 +0800

    Day 12 Task

---
+++ task12a.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isLower(string s) {
+    for (int i = 0; i < s.size(); ++i) {
+        if (!islower(s[i])) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void dfs(vector<vector<int>> &AL, vector<bool> visited, unordered_map<int, string> &umapRev, int curNode, int* numWays) {
+    if (umapRev[curNode] == "end") {
+        (*numWays)++;
+        return;
+    }
+    if (isLower(umapRev[curNode])) {
+        visited[curNode] = true;
+    }
+    for (auto &n: AL[curNode]) {
+        if (!visited[n] && umapRev[n] != "start") {
+            dfs(AL, visited, umapRev, n, numWays);
+        }
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    string line;
+    vector<vector<int>> AL;
+    int curNode = 0;
+    unordered_map<string, int> umap;
+    unordered_map<int, string> umapRev;
+    while (getline(cin, line)) {
+        string node1, node2;
+        for (int i = 0; i < line.size(); ++i) {
+            if (line[i] == '-') {
+                node1 = line.substr(0, i);
+                node2 = line.substr(i+1);
+            }
+        }
+        if (umap.find(node1) == umap.end()) {
+            umap[node1] = curNode;
+            umapRev[curNode] = node1;
+            curNode++;
+            AL.push_back(vector<int>());
+        }
+        if (umap.find(node2) == umap.end()) {
+            umap[node2] = curNode;
+            umapRev[curNode] = node2;
+            curNode++;
+            AL.push_back(vector<int>());
+        }
+        AL[umap[node1]].push_back(umap[node2]);
+        AL[umap[node2]].push_back(umap[node1]);
+    }
+    int numWays = 0;
+    int start = umap["start"];
+    vector<bool> visited(curNode, false);
+    dfs(AL, visited, umapRev, start, &numWays);
+    cout << numWays;
+    return 0;
+}
