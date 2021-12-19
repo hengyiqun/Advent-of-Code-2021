a7e51ed09709685172ab524684583a49cbafbf68
hengyiqun <hengyiqun@gmail.com>
2021-12-19 23:20:48 +0800

    Day 17 Task 2

--- task17b.cpp
+++ task17b.cpp
@@ -22,17 +22,20 @@ int main() {
     int minYi = y1, maxYi = -y1 - 1;
     int minXi = calculateX(x1), maxXi = x2;
         
-    int maxT, distX, distY;
+    int minT, maxT, distX, distY;
     set<pair<int, int>> ans;
     for (int xi = minXi; xi <= maxXi; ++xi) {
         for (int yi = minYi; yi <= maxYi; ++yi) {
             if (yi > 0) {
                 maxT = ((-1 - 2*yi) + sqrt((2*yi + 1) * (2*yi + 1) - 8 * y1)) / 2;
                 maxT += yi * 2 + 1;
+                minT = ((-1 - 2*yi) + sqrt((2*yi + 1) * (2*yi + 1) - 8 * y2)) / 2;
+                minT += yi * 2 + 1;
             } else {
-                maxT = ((1 - 2*yi) + sqrt((2*yi - 1) * (2*yi - 1) - 8 * y1)) / 2;
+                maxT = ((1 + 2*yi) + sqrt((2*yi + 1) * (2*yi + 1) - 8 * y1)) / 2;
+                minT = ((1 + 2*yi) + sqrt((2*yi + 1) * (2*yi + 1) - 8 * y2)) / 2;
             }
-            for (int t = 0; t <= maxT; ++t) {
+            for (int t = minT; t <= maxT; ++t) {
                 if (t >= xi) {
                     distX = (xi + 1) * xi / 2;
                 } else {
@@ -67,3 +70,25 @@ int main() {
     // cout << x1 << ' ' << x2 << ' ' << y1 << ' ' << y2;
     return 0;
 }
+
+
+/*
+
+
+For positive yi:
+After t1 = 2 * yi + 1, the vertical position of the probe will be back to starting value of 0.
+After another t2 seconds, let the probe be at -(y+1)-(y+2)-...-(y+t) =  y1
+Then (-y-1-y-t)*t/2 = y1        =>      t2 = ((-1-2*y) + sqrt((1+2*y)^2 - 8*y1)) / 2
+Therefore tmax = 2 * yi + 1 + ((-1 - 2*y) + sqrt((1 + 2*y)^2 - 8*y1)) / 2
+
+Similarly, tmin = 2 * yi + 1 + ((-1 - 2*y) + sqrt((1 + 2*y)^2 - 8*y2)) / 2
+
+
+For negative yi
+let y + (y-1) + ... + (y-t+1) = y1
+Then (y + y - t + 1) * t / 2 = y1       =>      t = ((1 + 2*y) + sqrt((1 + 2*y)^2 - 8*y1)) / 2
+Therefore tmax = ((1 + 2*y) + sqrt((1 + 2*y)^2 - 8*y1)) / 2
+
+Similarly, tmin = ((1 + 2*y) + sqrt((1 + 2*y)^2 - 8*y2)) / 2
+
+*/
